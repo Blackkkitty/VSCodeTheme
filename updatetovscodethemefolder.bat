9ee042dda9977a327b2d8c4af1171f783fda76df
I\blackkitty <xiaomaozaier@msn.cn>
2018-10-14 17:55:21 +0800

    auto update batch

--- updatetovscodethemefolder.bat
+++ updatetovscodethemefolder.bat
@@ -1,6 +1,7 @@
 @echo off
 :: 使用管理员权限运行
 set aimdir=C:\Program Files (x86)\Microsoft VS Code\resources\app\extensions
+
 for /f %%t in ('dir /b /a D .\theme-*') do (
     set/p _=Copy - %%t - <NUL
     xcopy /e /y /q /i ".\%%t" "%aimdir%\%%t"
